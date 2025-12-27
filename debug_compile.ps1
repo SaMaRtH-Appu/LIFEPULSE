bc9d3035e102c48bc68d7c53c03428a544628541
Samarth!! <samarth123mahesh@gmail.com>
2025-12-28 00:46:39 +0530

    Delete debug_compile.ps1

--- debug_compile.ps1
+++
@@ -1,8 +0,0 @@
-$JavaDir = Get-ChildItem -Path $PSScriptRoot -Filter "java-*" -Directory | Select-Object -First 1
-$MavenDir = "$PSScriptRoot\maven_tool\apache-maven-3.9.6"
-
-$env:JAVA_HOME = $JavaDir.FullName
-$env:M2_HOME = $MavenDir
-$env:PATH = "$($JavaDir.FullName)\bin;$MavenDir\bin;$env:PATH"
-
-mvn clean compile
